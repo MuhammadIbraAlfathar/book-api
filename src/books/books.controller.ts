9ce26a156ebefe69a6ba451b57d76782e4a3b688
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-08-31 14:27:41 +0700

    create method put, create findBookByIndex, and create updateBook

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { BooksService } from './books.service';
 
 @Controller('books')
@@ -17,5 +17,15 @@ export class BooksController {
     @Body('category') category: string,
   ) {
     return this.bookService.createBook(title, author, category);
+  }
+
+  @Put('/:id')
+  updateBook(
+    @Param('id') id: string,
+    @Body('title') title: string,
+    @Body('author') author: string,
+    @Body('category') category: string,
+  ) {
+    return this.bookService.updateBook(id, title, author, category);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
@@ -15,6 +15,24 @@ export class BooksService {
       title,
       author,
       category,
+      createdAt: new Date(),
+      updateAt: new Date(),
     });
+  }
+
+  updateBook(id: string, title: string, author: string, category: string) {
+    const bookIndex = this.findBookById(id);
+    this.books[bookIndex].title = title;
+    this.books[bookIndex].author = author;
+    this.books[bookIndex].category = category;
+  }
+
+  findBookById(id: string) {
+    const bookIndex = this.books.findIndex((book) => book.id === id);
+    if (bookIndex === -1) {
+      throw new NotFoundException(`Book with id ${id} is not found`);
+    }
+
+    return bookIndex;
   }
 }
