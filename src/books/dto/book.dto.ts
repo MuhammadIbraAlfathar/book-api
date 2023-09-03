cba91cf3e7fe410b351b914ce2fbf506de840abb
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-09-03 14:45:25 +0700

    add validation pipe

--- src/books/dto/book.dto.ts
+++ src/books/dto/book.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsInt } from 'class-validator';
+
 export class BooksDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   author: string;
+
+  @IsNotEmpty()
   category: string;
+
+  @IsNotEmpty()
+  @IsInt()
   year: string;
 }
