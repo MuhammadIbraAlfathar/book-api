516da128347554bbf6e8edd4fe4b9e96162cc934
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-09-04 13:50:23 +0700

    create repository and filter books

---
+++ src/books/repository/book.repository.ts
@@ -0,0 +1,40 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Book } from '../entity/book.entity';
+import { FilterBookDto } from '../dto/filter-book.dto';
+
+@EntityRepository(Book)
+export class BookRepository extends Repository<Book> {
+  async getBooks(filter: FilterBookDto): Promise<Book[]> {
+    const { title, author, category, min_year, max_year } = filter;
+
+    const query = this.createQueryBuilder('book');
+
+    if (title) {
+      query.andWhere('lower(book.title) LIKE :title', {
+        title: `%${title.toLocaleLowerCase()}%`,
+      });
+    }
+
+    if (author) {
+      query.andWhere('lower(book.author) LIKE :author', {
+        author: `%${author.toLocaleLowerCase()}%`,
+      });
+    }
+
+    if (category) {
+      query.andWhere('lower(book.category) LIKE :category', {
+        category: `%${category.toLocaleLowerCase()}%`,
+      });
+    }
+
+    if (min_year) {
+      query.andWhere('book.year >= min_year', { min_year });
+    }
+
+    if (max_year) {
+      query.andWhere('book.year <= max_year', { max_year });
+    }
+
+    return await query.getMany();
+  }
+}
