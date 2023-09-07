a02bd0c05b60e743f7f8c3370c6efbb41711c9fa
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-09-07 14:57:24 +0700

    create method createRefreshToken in repository

--- src/auth/repository/refresh-token.repository.ts
+++ src/auth/repository/refresh-token.repository.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { RefreshToken } from '../entity/refresh-token.entity';
+import { User } from 'src/users/entity/user.entity';
 
 @Injectable()
 export class RefreshTokenRepository extends Repository<RefreshToken> {
   constructor(private dataSource: DataSource) {
     super(RefreshToken, dataSource.createEntityManager());
+  }
+  async createRefreshToken(user: User, ttl: number): Promise<RefreshToken> {
+    const refreshToken = this.create();
+    const expiredAt = new Date();
+    expiredAt.setDate(expiredAt.getDate() + ttl);
+    refreshToken.isRevoked = false;
+    refreshToken.expiredAt = expiredAt;
+    refreshToken.user = user;
+
+    return await refreshToken.save();
   }
 }
