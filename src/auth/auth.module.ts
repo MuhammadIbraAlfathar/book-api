f1b945d42a0e0bf1d1ed7bde94bf09d947f0a51b
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-09-07 11:08:09 +0700

    create access token and controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { jwtConfig } from 'src/books/config/jwt.config';
 import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [JwtModule.register(jwtConfig), UsersModule],
   providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { LoginDto } from './dto/login.dto';
 import { LoginResponse } from './interface/login-response.interface';
 import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/entity/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +12,7 @@ export class AuthService {
     private readonly userService: UsersService,
   ) {}
 
-  async login(loginDto: LoginDto): Promise<LoginResponse> {
+  async login(loginDto: LoginDto): Promise<string> {
     const { email, password } = loginDto;
 
     const user = await this.userService.validateUser(email, password);
@@ -19,5 +20,19 @@ export class AuthService {
     if (!user) {
       throw new UnauthorizedException('Wrong email and password');
     }
+
+    const access_token = this.createAccessToken(user);
+
+    return access_token;
+  }
+
+  async createAccessToken(user: User): Promise<string> {
+    const payload = {
+      sub: user.id,
+    };
+
+    const access_token = await this.jwtService.signAsync(payload);
+
+    return access_token;
   }
 }
